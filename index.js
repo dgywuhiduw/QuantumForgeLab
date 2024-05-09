3dcb29dd1cb54ef1059dad10f90bf31d86bcdf63
dgywuhiduw <gs1jesshelz@hotmail.com>
2024-05-09 12:57:07 +0700

    init

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return maxProfit;
-}
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  }
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
